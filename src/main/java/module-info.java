2d2fa2c0440a8a7e6cf28132cac713f85788edff
Lucas ARNU <lucas.arnu1@etu.univ-lorraine.fr>
2023-04-16 15:19:31 +0200

    suppression ancien fichiers

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module univ_lorraine.iut.java.PrivateChat {
-    requires javafx.controls;
-    requires javafx.fxml;
-    
-    opens univ_lorraine.iut.java.privatechat.controller to javafx.fxml;
-    opens univ_lorraine.iut.java.privatechat to javafx.fxml;
-    exports univ_lorraine.iut.java.privatechat;
-
-}
